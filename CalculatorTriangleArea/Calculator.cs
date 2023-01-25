da386bbfc533aef948ce1dd0db090ceb208e11cb
Ruslan Tverezenko <68191748+ruslantverezenko@users.noreply.github.com>
2023-01-26 00:32:33 +0300

    Summary
    Description

---
+++ CalculatorTriangleArea/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTriangleArea
+{
+    class Calculator
+    {
+        public double CalcTriangleSquare(double ab, double bc, double ac)
+        {
+            double p = (ab + bc + ac) / 2;
+            return Math.Sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        }
+
+        public double CalcTriangleSquare(double b, double h)
+        {
+            return 0.5 * b * h;
+        }
+
+        public double CalcTriangleSquare(double ab, double ac, int alpha)
+        {
+            double rads = alpha * Math.PI / 180;
+            return 0.5 * ab * ac * Math.Sin(rads);
+        }
+    }
+}
---
+++ CalculatorTriangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            double square1 = calc.CalcTriangleSquare(10, 20);
+            double square2 = calc.CalcTriangleSquare(40, 20, 30);
+            double square3 = calc.CalcTriangleSquare(10, 20, 50);
+
+            Console.WriteLine($"Square1 = {square1}\nSquare2 = {square2}\nSquare3 = {square3} ");
+        }
+    }
+}
